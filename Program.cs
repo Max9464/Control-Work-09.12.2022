f794c2f81dbb0a355684aa7a7e17d1f209264a6e
Kinder <kindermaks@mail.ru>
2022-12-29 02:47:27 +0300

    Create metod

--- Program.cs
+++ Program.cs
@@ -4,4 +4,15 @@ string[] array1 = new string[5] {"ef$", "456", "helo", "wld", "rdws"};
 string[] array2 = new string[array1.Length];
 
 void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 
